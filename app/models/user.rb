2549539fd1337396dde8c7be7bab9e6104308798
denis281542 <deniskuimov@mail.ru>
2019-12-11 23:25:08 +0500

    Edit user helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   
-  before_action :authenticate_user!
+  before_commit :authenticate_user!
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
